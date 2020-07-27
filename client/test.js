f973407d28edef48faf7322ca56fd5d9e2ab6ceb
mixomatric <ankit.vayuz@gmail.com>
2020-07-27 15:19:12 +0530

    Adding Todo list item insertion

--- client/test.js
+++ client/test.js
@@ -1,4 +1,7 @@
 import { Todo } from './../collections/collections';
+Template.hello.onRendered(function(){
+	Meteor.subscribe("fetch_all_tasks")
+})
 Template.hello.helpers({
 
 	fetch_all_posts:function(){
@@ -7,5 +10,33 @@ Template.hello.helpers({
 		console.log("data");
 		return data;
 		},
+
+})
+
+
+Template.hello.events({
+	"click #submit_btn":function(event){
+		event.preventDefault();
+		var input = $("#simple_input").val()
+		if(input == ""){
+			alert("Please enter the task name");
+		}else{
+			alert("Entered Task name: " + input);
+			var obj ={};
+			obj.task_name = input;
+			$("#loading_text").removeClass("display_hidden");
+
+			Meteor.call("add_task", obj,function(error,result){
+				if(error){
+					alert("Something went wrong!");
+				}else{
+					$("#loading_text").addClass("display_hidden");
+					$("#simple_input").val("")	
+				}
+			});
+					
+			
+		}
+	}
 
 })
--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
 import {Todo} from './../collections/collections.js';
 
 Meteor.startup(function(){
-	console.log("Server Started");
-	console.log("Server Started Agasdaain");
-	Todo.insert({ name: "David", score: 0 });
+	// Todo.insert({ name: "David", score: 0 });
+})
+
+Meteor.publish("fetch_all_tasks",function(){
+	return Todo.find({});
+})
+
+Meteor.methods({
+	"add_task":function(obj){
+		var result = Todo.insert({ name: obj.task_name, score: 0 });
+		return result;
+	}
+
 })
